4a140877984c5f86e2b8c9cf7125f3296f0f571f
Dileepa_Tech <103705712+DileepaTech@users.noreply.github.com>
2023-05-14 09:52:21 +0530

    Add files via upload

---
+++ plugins/econ-daily.js
@@ -0,0 +1,43 @@
+
+//import db from '../lib/database.js'
+
+const rewards = {
+  exp: 9999,
+  money: 4999,
+  potion: 5,
+}
+const cooldown = 86400000
+let handler = async (m,{ conn} ) => {
+  let user = global.db.data.users[m.sender]
+  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
+  let text = ''
+  for (let reward of Object.keys(rewards)) {
+    if (!(reward in user)) continue
+    user[reward] += rewards[reward]
+    text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
+  }
+  conn.sendButton(m.chat,'*––––––『 DAILY 』––––––*', text.trim(), null, [['Adventure', '.adventure'], ['Weekly', '.weekly']],m)
+  user.lastclaim = new Date * 1
+}
+handler.help = ['daily', 'claim']
+handler.tags = ['xp']
+handler.command = /^(daily|claim)$/i
+
+handler.cooldown = cooldown
+
+export default handler
+
+
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " Horas " + minutes + " Minutos"
+}
